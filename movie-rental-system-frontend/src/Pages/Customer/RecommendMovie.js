d17b4dac7999208ecc1e2714cbe06f8b395d1d15
YigitEkin <yigit.ekin@ug.bilkent.edu.tr>
2022-05-15 05:01:07 +0300

    added recommend movie page

--- movie-rental-system-frontend/src/Pages/Customer/RecommendMovie.js
+++ movie-rental-system-frontend/src/Pages/Customer/RecommendMovie.js
@@ -26,65 +26,97 @@ const Recommendmovie = () => {
   const [friends, setFriends] = useState(tempUsers);
   const title = useRef(null);
   const friend = useRef(null);
+  const message = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const recommendation = {
+      recommending_user: state.user_name,
+      recommended_user: friend.current.value,
+      movie_title: title.current.value,
+      message: message.current.value,
+    };
+    //TODO: send recommendation to backend
+  };
 
   return (
     <>
       <Navbar name={state.user_name} />
       <div className="container mt-5">
         <div class="jumbotron jumbotron-fluid">
-          <div class="container">
-            <h1 class="display-4 text-center">Recommend A Movie To Friends</h1>
-            <div className="row" style={{ backgroundColor: "red" }}>
-              <div className="col-6">
+          <div class="container px-5">
+            <form onSubmit={handleSubmit}>
+              <h1 class="display-4 text-center">
+                Recommend A Movie To Friends
+              </h1>
+              <div className="row">
+                <div className="col-6">
+                  <div className="form-group">
+                    <label
+                      htmlFor="exampleInputEmail1"
+                      className="text-center text-dark text-lg"
+                    >
+                      Movie Title:
+                    </label>
+                    <input
+                      type="text"
+                      className="form-control"
+                      id="exampleInputEmail1"
+                      ref={title}
+                      aria-describedby="emailHelp"
+                      placeholder="Example: The Godfather"
+                    />
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="form-group">
+                    <>
+                      <div className="container">
+                        <label
+                          className="text-center text-dark text-lg"
+                          htmlFor="DeleteUser"
+                        >
+                          Friends:
+                        </label>
+                        <select
+                          name="friends"
+                          ref={friend}
+                          className="form-control"
+                          id=""
+                        >
+                          {friends.map((user) => {
+                            return (
+                              <option key={user.id} value={user.user_name}>
+                                {user.user_name}
+                              </option>
+                            );
+                          })}
+                        </select>
+                      </div>
+                    </>
+                  </div>
+                </div>
+              </div>
+              <div>
                 <div className="form-group">
-                  <label
-                    htmlFor="exampleInputEmail1"
-                    className="text-center text-light text-lg"
-                  >
-                    Movie Title:
+                  <label htmlFor="exampleInputPassword1" className="text-lg">
+                    Description:
                   </label>
-                  <input
-                    type="text"
+                  <textarea
+                    type="number"
+                    placeholder="Give a brief description of the movie"
+                    rows={7}
+                    min={1920}
                     className="form-control"
-                    id="exampleInputEmail1"
-                    ref={title}
-                    aria-describedby="emailHelp"
-                    placeholder="Example: The Godfather"
+                    id="exampleInputPassword1"
+                    ref={message}
                   />
                 </div>
               </div>
-              <div className="col-6">
-                <div className="form-group">
-                  <>
-                    <div
-                      className="container"
-                      style={{ backgroundColor: "red" }}
-                    >
-                      <label
-                        className="text-center text-light text-lg"
-                        htmlFor="DeleteUser"
-                      >
-                        Friends:
-                      </label>
-                      <select
-                        name="friends"
-                        ref={friend}
-                        className="form-control"
-                        id=""
-                      >
-                        {friends.map((user) => {
-                          return (
-                            <option key={user.id} value={user.user_name}>
-                              {user.user_name}
-                            </option>
-                          );
-                        })}
-                      </select>
-                    </div>
-                  </>
-                </div>
-              </div>
-            </div>
+              <button type="submit" className="btn btn-block btn-red">
+                Recommend Movie
+              </button>
+            </form>
           </div>
         </div>
       </div>
